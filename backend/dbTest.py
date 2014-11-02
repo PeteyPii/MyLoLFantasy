75678aab437ce4524740f6af4cc7f3ce9d6994a7
Patrick Wrobel <pwrobel@uwaterloo.ca>
2014-11-02 05:50:10 -0500

    Added dbTester

---
+++ backend/dbTest.py
@@ -0,0 +1,10 @@
+import databaseMethods as db
+
+# db.create_user("Patty", "password", "BasicBananas")
+db.create_group("Patty", "GROUP_NAME", ["BasicBananas", "Wonkeee"])
+db.create_group("Patty", "GROUP_NAME_2", ["BasicBananas", "Pulsefire Annie"])
+groups = db.get_groups_in("Patty")
+# print(groups)
+# for group in groups:
+#   print(db.get_group_name(group))
+#   print(db.get_group_data(group))
