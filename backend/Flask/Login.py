76145f97e36c5219d4259f28914504ee0aefaa0a
balajisankar1 <balajisankar1@gmail.com>
2014-11-02 01:25:29 -0500

    Changed Submit Page so that nice css also works

--- backend/Flask/Login.py
+++ backend/Flask/Login.py
@@ -1,24 +1,38 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask.ext.login import login_user , logout_user , current_user , login_required, LoginManager
+import user
+
 app = Flask(__name__)
 
+app.secret_key = 'b\xb2\xd9\x81\xaf\xea\xfe\xbb\xe3\x1e\xebt3\x06\x07\x9f\xc9\xd1`\xbdG\xf1\xf8;-'
+
+#login_manager = LoginManager()
+#login_manager.init_app(app)
+#login_manager.login_view = 'login'
 
 @app.route('/signup.html', methods=['GET', 'POST'])
-def Login():
+def AuthenticateLogin():
   error = None
   print(request.form)
   if request.method == 'POST':
-    if request.form['login'] != 'admin' or request.form['password'] != 'admin':
-      print("we got here")
-      error = 'Invalid Credentials. Please Try Again'
-    else:
-      return render_template('home.html')
+    username = request.form['username']
+    password = request.form['password']
+    confirmPassword = request.form['confirmPass']
   return render_template('signup.html', error=error)
 
 @app.route('/')
-def signup():
-    return render_template('home.html')
+def home():
+ return render_template('home.html')
 
 
- 
+@app.route('/home.html')
+def loadhome():
+ return render_template('home.html')
+
+#@login_manager.user_loader
+#def load_user(id):
+#  return user.User.getUser(username)
+
+
 if __name__ == '__main__':
   app.run(debug=True)
