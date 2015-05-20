0889d19a2c2960c03ed23f0d16e120f005500e52
balajisankar1 <balajisankar1@gmail.com>
2015-05-19 23:43:18 -0400

    Started Work on Leagues Page

--- route.js
+++ route.js
@@ -291,6 +291,31 @@ router.post('/CreateLeague', function(req, res) {
 
 module.exports = router;
 
+router.get("/League_:leagueId", function(req, res) {
+  if (!req.user) {  
+    redirectRequireLogin(req, res);
+    return;
+  }
+  res.locals.leagueId = req.params.leagueId
+  req.app.locals.db.getLeague(res.locals.leagueId).then(function(league) {
+    if (league){
+      res.locals.league = league;
+    } else {
+      error = "Cannot find league with id " + leagueId; 
+      req.flash('cannotFindLeagueError', leagueId);
+      throw error;
+    }
+    console.log(league);
+    res.render('league');
+  }).fail(function(reason){
+    req.flash('loadLeagueError', leagueId)
+    res.send({
+      success: false
+    })
+    res.render('league');
+  }).done();
+});
+
 function redirectRequireLogin(req, res) {
   req.flash('loginRequired', true);
   req.flash('loginError', 'You must be logged in to view that!');
