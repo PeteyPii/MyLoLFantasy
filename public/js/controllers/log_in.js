df3882f805101dab39c8092454d73a70675007c6
Patrick Wrobel <pwrobel@uwaterloo.ca>
2016-07-10 05:19:52 -0400

    Fixed display username not updating properly in nav bar

--- public/js/controllers/log_in.js
+++ public/js/controllers/log_in.js
@@ -10,6 +10,10 @@ app.controller('LogInController', ['$rootScope', '$scope', '$http', '$location',
 
     $scope.displayUsername = authService.username();
 
+    $scope.$on('profileChanged', function(event) {
+      $scope.displayUsername = authService.username();
+    });
+
     var isWorking = false;
 
     $scope.logIn = function() {
