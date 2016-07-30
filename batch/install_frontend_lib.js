7f89f1062ca959b4916e68b822c6a158e0dca432
Patrick Wrobel <pwrobel@uwaterloo.ca>
2016-07-30 14:58:26 -0400

    Fixed a few minor problems with install frontends script

--- batch/install_frontend_lib.js
+++ batch/install_frontend_lib.js
@@ -10,7 +10,9 @@ var files = [
   'angular-route/angular-route.js',
   'angular/angular.js',
   'bootstrap/dist/css/bootstrap-theme.css',
+  'bootstrap/dist/css/bootstrap-theme.css.map',
   'bootstrap/dist/css/bootstrap.css',
+  'bootstrap/dist/css/bootstrap.css.map',
   'bootstrap/dist/fonts/glyphicons-halflings-regular.eot',
   'bootstrap/dist/fonts/glyphicons-halflings-regular.svg',
   'bootstrap/dist/fonts/glyphicons-halflings-regular.ttf',
@@ -18,12 +20,12 @@ var files = [
   'bootstrap/dist/fonts/glyphicons-halflings-regular.woff2',
   'bootstrap/dist/js/bootstrap.js',
   'font-awesome/css/font-awesome.css',
-  'font-awesome/fonts/FontAwesome.otf',
   'font-awesome/fonts/fontawesome-webfont.eot',
   'font-awesome/fonts/fontawesome-webfont.svg',
   'font-awesome/fonts/fontawesome-webfont.ttf',
   'font-awesome/fonts/fontawesome-webfont.woff',
   'font-awesome/fonts/fontawesome-webfont.woff2',
+  'font-awesome/fonts/FontAwesome.otf',
   'jquery/dist/jquery.js',
   'ng-dialog/css/ngDialog.css',
   'ng-dialog/js/ngDialog.js',
@@ -42,6 +44,7 @@ bower.commands.install(undefined, undefined, { cwd: path.join(__dirname, '..') }
         fs.copySync(path.join(__dirname, '../bower_components', file), path.join(__dirname, '../public/js/lib/', path.basename(file)));
         break;
       case '.css':
+      case '.map':
         fs.copySync(path.join(__dirname, '../bower_components', file), path.join(__dirname, '../public/css/', path.basename(file)));
         break;
       case '.otf':
