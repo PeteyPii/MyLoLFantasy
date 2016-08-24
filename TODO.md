8ce5f3a268ee24c934f94034ab8ee73d6653a26a
Patrick Wrobel <pwrobel@uwaterloo.ca>
2016-08-24 05:32:26 -0400

    Added gulp task to build a minified version of the site

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 # 2.0.0 TODO List
 - Mobile prettiness
-- Build script (copy, concat, minify, clean)
 - Update README
 - Cache static resources better
 - Autofill input style
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,30 @@
 var childProcess = require('child_process');
 var path = require('path');
 
+var clean = require('gulp-clean');
+var cleanCss = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var copy = require('gulp-copy');
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var jshint = require('gulp-jshint');
 var less = require('gulp-less');
 var lesshint = require('gulp-lesshint');
+var merge = require('merge-stream');
 var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 
 var bowerInstall = require('./lib/bower_install.js');
 var logger = require('./lib/logger.js');
 var settings = require('./lib/settings.js');
 var version = require('./lib/version.js');
+var frontEndDependencies = require('./lib/front_end_deps.js');
 
 var jadeLocals = {
   settings: settings,
   version: version,
+  deps: frontEndDependencies,
 };
 
 var paths = {};
@@ -27,8 +35,15 @@ paths.jade = 'jade/**/*.jade';
 paths.html = 'public/';
 paths.serverJs = ['server.js', 'lib/**/*.js'];
 paths.publicJs = ['public/js/**/*.js', '!public/js/lib/**/*.js'];
-paths.server = concatenateItems(paths.serverJs, '*.json');
+paths.server = concatenateItems(paths.serverJs, '*.json', '!package.json', '!bower.json');
 paths.js = concatenateItems('gulpfile.js', paths.serverJs, paths.publicJs);
+paths.misc = ['node_modules/**/*'];
+paths.assets = ['public/imgs/**/*', 'public/fonts/**/*', 'public/**/*.html'];
+paths.public = 'public/';
+paths.build = 'dist/';
+paths.buildJsHead = 'dist/public/js/bundle_head.js';
+paths.buildJsFinal = 'dist/public/js/bundle_final.js';
+paths.buildCss = 'dist/public/css/bundle.css';
 
 function concatenateItems() {
   var itemList = [];
@@ -145,5 +160,59 @@ gulp.task('lint_less', function() {
 });
 
 gulp.task('lint', ['lint_js', 'lint_less']);
+
+gulp.task('build_server', function() {
+  return gulp.src(paths.server)
+    .pipe(copy(paths.build));
+});
+
+gulp.task('build_misc', function() {
+  return gulp.src(paths.misc)
+    .pipe(copy(paths.build));
+});
+
+gulp.task('build_assets', ['jade'], function() {
+  return gulp.src(paths.assets)
+    .pipe(copy(paths.build));
+});
+
+gulp.task('build_js', function() {
+  var headFiles = frontEndDependencies.headJs.map(function(fileName) {
+    return paths.public + fileName;
+  });
+  var finalFiles = frontEndDependencies.finalJs.map(function(fileName) {
+    return paths.public + fileName;
+  });
+
+  var head = gulp.src(headFiles)
+    .pipe(concat(paths.buildJsHead))
+    .pipe(uglify())
+    .pipe(gulp.dest('.'));
+  var final = gulp.src(finalFiles)
+    .pipe(concat(paths.buildJsFinal))
+    .pipe(uglify())
+    .pipe(gulp.dest('.'));
+  return merge(head, final);
+});
+
+gulp.task('build_css', ['less'], function() {
+  var files = frontEndDependencies.css.map(function(fileName) {
+    return paths.public + fileName;
+  });
+
+  return gulp.src(files)
+    .pipe(concat(paths.buildCss))
+    .pipe(cleanCss({
+      keepSpecialComments: 0,
+    }))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('build', ['build_server', 'build_misc', 'build_assets', 'build_js', 'build_css']);
+
+gulp.task('clean', function() {
+  return gulp.src(paths.build, { read: false })
+    .pipe(clean());
+});
 
 gulp.task('default', ['watch']);
