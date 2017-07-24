ecbf6a945f1721a737e76bb3e8507fcb5c9ab2e6
Patrick Wrobel <craptik@gmail.com>
2017-07-24 05:47:28 -0400

    Finished script to add account ids to every league and upon new league creations

--- batch/update_db.js
+++ batch/update_db.js
@@ -1,13 +1,27 @@
+var Q = require('q');
+
 var DB = require('../lib/database.js');
 var logger = require('../lib/logger.js');
+var LoL = require('../lib/lol.js');
+var Redis = require('../lib/redis.js');
 
 try {
   var db = new DB();
+  var redis = new Redis();
+  var lol = new LoL(redis);
 
-  db.getVersion().then(function(version) {
-    return db.updateDb(version);
+  redis.init(true, true, true).then(function() {
+    return lol.init();
+  }).then(function() {
+    return db.getVersion();
+  }).then(function(version) {
+    return db.updateDb(version, lol);
   }).fail(logger.logException).fin(function() {
     return db.deinit();
+  }).fin(function() {
+    return lol.deinit();
+  }).fin(function() {
+    return redis.deinit();
   }).fail(logger.logException).done();
 } catch (err) {
   logger.logException(err);
